8597018f8979760c0312ed586f2b39d7d4976b9c
Clarisse Erpeldinger <43817141+erpeldinger@users.noreply.github.com>
2019-05-24 10:21:50 +0200

    Début d'écriture des classes LaunchTest et TestPerformance

--- src/test/org/insa/performance/LaunchTest.java
+++ src/test/org/insa/performance/LaunchTest.java
@@ -6,7 +6,33 @@ package org.insa.performance;
  * plusieurs tests sur différentes cartes
 */
 
-
 public class LaunchTest {
+	
+	@Test
+	public void lancementTest() {
+		//On lance les tests avec les différentes cartes, évaluations, algo
+		
+		ArrayList<TestPerformance> tableauTest = new Arraylist<TestPerformance>();
+		
+		//Dijkstra
+		//Boucle pour le type d'évaluation (temps, distance)
+		for (int i=0; i<2; i++) {
+			//Boucle pour les cartes
+			for (int l=0; i<cartes.size()) {
+				tableauTest.add(new TestPerformance);
+				tableauTest.get(i).doRun(dataDirectory.get(0)+cartes.get(k), i, 0);
+			//Dans le doRun : nomFichierEcritureDonnees, nomFichierEcritureREsultats, typeEvaluation, algo
+			}			
+		}
+		
+		//AStar
+		//Boucle pour les cartes
+		for (int l=0; i<cartes.size) {
+			tableauTest.add(new TestPerformance);
+			tableauTest.get(i).doRun(dataDirectory.get(0)+cartes.get(k), i);
+		//Dans le doRun : nomFichierEcritureDonnees, nomFichierEcritureREsultats, typeEvaluation, algo
+		}			
+	}
+	
 
 }
--- src/test/org/insa/performance/TestPerformance.java
+++ src/test/org/insa/performance/TestPerformance.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 public class PerformanceTest {
 
 	public void doRun() {
-		
+		//Dans le doRun : nomFichierEcritureDonnees, nomFichierEcritureREsultats, typeEvaluation, algo
 	}
 	
 
