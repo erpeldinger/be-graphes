1078ad054a87c8fdfb849ab657b6062165ee9c85
Clarisse Erpeldinger <43817141+erpeldinger@users.noreply.github.com>
2019-05-24 11:27:51 +0200

    suite écriture des classes LaunchTest et TestPerformance

--- src/readme.txt
+++ src/readme.txt
@@ -5,8 +5,8 @@ A corriger :
 - gestion de l'exception IOException dans DijkstraScenarioTest à vérifier
 
 
-- J'ai interverti le type pour distance (0) et temps (1)
+- Vérifier tests JUnit sur AStr et Dijkstra --> erreurs
 - Penser à faire le calcul du CPU (actuellement dans EcritureDonnees mais ce n'est pas le bon endroit)
-
+- Penser à récupérr caractéristiques machine où on lance les tas (CPU)
 
 Rq : Je n'ai fait que deux types de test (distance et temps) sur une seule carte mais on peut en faire plein d'autres !
--- src/test/org/insa/performance/TestPerformance.java
+++ src/test/org/insa/performance/TestPerformance.java
@@ -9,10 +9,26 @@ import org.junit.Test;
  *
 */
 
+protected ArrayList<>
+
 public class TestPerformance {
 
-	public void doRun() {
-		//Dans le doRun : nomFichierEcritureDonnees, nomFichierEcritureREsultats, typeEvaluation, algo
+	public void doRun(String nomCarte, int typeEval, int algo) {
+		//Dans le doRun : nomCarte, typeEvaluation, algo
+	
+	//On appelle dans l'ordre 
+		//- EcritureFichier (EcritureDOnees) -> écrit les donnees du fichier txt à partir des infos carte
+		//- LectureFichier (EcritureResultats) -> lis les données du fichier txt
+		//- EcritureCalculs (EcritureResultats) -> exécute algo + écrit fichier txt
+	
+		//VOIR COMMENT C'EST FAIT DANS ERCRITURERESULTATS
+		EcritureDonnees writeData = new EcritureDonnees(nomCarte, typeEval);
+		EcritureResultats writeResult = new EcritureResultats(nomCarte, typeEval, algo);
+		
+		
+		//String cheminDonnee = writeData.getNomFichier();
+		//String cheminResultat = writeResult.getNomFichier().get();
+		
 	}
 	
 
